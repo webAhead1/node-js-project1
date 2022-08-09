73f2b228fd8334c900fdda8c9db965f624dfb7ee
“Maysan <maysanir@mta.ac.il>
2022-08-09 22:34:39 +0300

    css part1

--- workshop/templates.js
+++ workshop/templates.js
@@ -7,18 +7,24 @@ function layout(content) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>health app</title>
         <link rel="stylesheet" href="/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,400;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </head>
       <body>
-        <header>
-          <nav>
-            <a href="/">home</a>
-            <a href="/log-in">Log in</a>
-            <a href="/history">history</a>
-            <a href="/details">Write details</a>
-            <a href="/results">Results</a>
-            <a href="/log-out">Log out</a>
-          </nav>
-        </header>
+      <div class="hero">
+      <nav>
+          <h2 class="logo">Asma's<span> HealthWeb</span></h2>
+          <ul>
+              <a href="/"class="btn">Home</a>
+              <a href="/log-in"class="btn">Log in</a>
+              <a href="/history"class="btn">History</a>
+              <a href="/details"class="btn">Write Details</a>
+              <a href="/results"class="btn">Results</a>
+              <a href="/log-out"class="btn">Log Out</a>
+          </ul>
+      </nav>
         ${content}
       </body>
     </html>
@@ -27,7 +33,7 @@ function layout(content) {
 
 function logOut(email) {
   return layout(/*html */ `
-    <h1>Good Bye ${email}}</h1> 
+    <h1 class="bye">Good Bye ${email}}</h1> 
     <p>You will be redirected in 3 seconds</p>
     <script>
         var timer = setTimeout(function() {
@@ -37,40 +43,53 @@ function logOut(email) {
 }
 function home() {
   return layout(/*html */ `
-    <h1>Welcome to our HealthWeb<\h1>
-    <h2>our web will calculate Your Body Mass Index and we will give you informations and some tips depending on the results ;)</h2> `);
+    <div class="homepage">
+    <h1>Welcome to our <span>HealthWeb</span></h1>
+    <h2>our web will calculate Your Body Mass Index </h2>
+    <h2>and we will give you informations and some tips depending on the results ;)</h2> `);
+    
 }
 function logIn() {
   return layout(/*html */ `
+  <div class="signin">
     <h2>Please Log in to your account first</h2>
     <form action="/log-in" method="POST">
-    <label for="email">
-    Your email<span aria-hidden="true">*</span>
-    </label>
-    <input id="email" type="email" name="email" required>
-    <label for="password">
-    Your password<span aria-hidden="true">*</span>
-    </label>
-    <input id="password" type="password" name="password" minlength="8" required>
-    <button type="submit">Log in</button>
+    <form id="emailin">
+    <p>  
+    <input id="email" type="email" name="email" placeholder="Example@whatever.com" required><i class="validation"></i>
+    </p>
+    <p>
+    <input id="password" type="password" name="password" placeholder="Enter Your Password" minlength="8" required><i class="validation"></i>
+    </p>
+    <p>
+    <input type="submit" id="login" value="Login">
+    </p>
     </form>
-    <h6><a href="http://localhost:3000/sign-up"> If you don't have an account yet</a></h6>
+    </form>
+    <div id="create-account-wrap">
+    <p><a href="http://localhost:3000/sign-up"> If you don't have an account yet</a></p>
+    </div><!--create-account-wrap-->
+    </div><!--login-form-wrap-->
   `);
 }
 function signUp() {
   return layout(/*html */ `
+  <div class="signuptitle">
   <h2>Please Sign Up here</h2>
   <form action="/sign-up" method="POST">
-  <label for="email">
-  Your email<span aria-hidden="true">*</span>
-  </label>
-  <input id="email" type="email" name="email" required>
-  <label for="password">
-  Your password<span aria-hidden="true">*</span>
-  </label>
-  <input id="password" type="password" name="password" minlength="8" required>
-  <button type="submit">Sign Up</button>
+  <form id="whatever">
+  <p>
+  <input id="email" type="email" name="email" placeholder ="Enter Your Email" required><i class="crying"></i>
+  </p>
+  <p>
+  <input id="password" type="password" name="password" placeholder="Create Your Password" minlength="8" required><i class="crying"></i>
+  </p>
+  <p>
+  <input type="submit" id="signup" value"Signup">
+  </p>
   </form>
+  </form>
+  </div>
   `);
 }
 function details() {
@@ -106,6 +125,7 @@ function results(name, bmi, x) {
   return layout(/*html */ `
   <h1>Hey ${name}</h1>
   <h2>Your Body Mass Index is ${bmi} This is considered: ${x}</h2>
+  <img src=" alt="Icons">
   `);
 }
 
